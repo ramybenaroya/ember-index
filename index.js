ed95e6a3e4130c3b19cb28d8e8a5e441e033b5d3
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-05-18 00:28:18 +0300

    https://github.com/ramybenaroya/ember-index/issues/6

--- index.js
+++ index.js
@@ -82,23 +82,25 @@ module.exports = {
 		var returnedTree = tree,
 			renamedIndexTree,
 			indexTree,
+			treesToMerge = [],
 			indexFiles = ['index.html'];
 
 		if (this.app.env !== 'production'){
 			indexFiles.push('tests/index.html');
 		}
 
-		if (this.options.enabled && this.options.output && type === 'all') {
-
-			renamedIndexTree = renameFiles(funnel(tree, {
-				srcDir: '.',
-				files: ['index.html'],
-				destDir: '.'
-			}), {
-				transformFilename: function() {
-					return this.options.output;
-				}.bind(this)
-			});
+		if (this.options.enabled && type === 'all') {
+			if (this.options.output) {
+				renamedIndexTree = renameFiles(funnel(tree, {
+					srcDir: '.',
+					files: ['index.html'],
+					destDir: '.'
+				}), {
+					transformFilename: function() {
+						return this.options.output;
+					}.bind(this)
+				});	
+			}
 
 			indexTree = funnel(tree, {
 				srcDir: '.',
@@ -114,14 +116,16 @@ module.exports = {
 				var injectedContentRegExp = new RegExp(startMarker + '([\\s\\S])*' + endMarker, 'g');
 
 				this._handleIdDepracation(content);
-
-				renamedIndexTree = replaceString(renamedIndexTree, {
-					files: [this.options.output],
-					pattern: {
-						match: content.includeInOutput ? markersRegExp : injectedContentRegExp,
-						replacement: ''
-					}
-				});
+				
+				if (this.options.output && renamedIndexTree) {
+					renamedIndexTree = replaceString(renamedIndexTree, {
+						files: [this.options.output],
+						pattern: {
+							match: content.includeInOutput ? markersRegExp : injectedContentRegExp,
+							replacement: ''
+						}
+					});	
+				}
 
 				indexTree = replaceString(indexTree, {
 					files: indexFiles,
@@ -133,7 +137,13 @@ module.exports = {
 
 			}, this);
 
-			returnedTree = mergeTrees([tree, renamedIndexTree, indexTree], {
+			treesToMerge.push(tree);
+			if (this.options.output && renamedIndexTree) {
+				treesToMerge.push(renamedIndexTree)
+			}
+			treesToMerge.push(indexTree);
+
+			returnedTree = mergeTrees(treesToMerge, {
 				overwrite: true
 			});
 		}
