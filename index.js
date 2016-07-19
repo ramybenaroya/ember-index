dc4a51aaa70e5336edc140aef5ded21d4d1757b8
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-07-19 20:12:50 +0300

    support windows path

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ module.exports = {
 					appPath = 'app';
 				}
 
-				if (new RegExp('^' + rootPath).test(appPath)){
+				if (new RegExp('^' + rootPath).test(appPath) || appPath.indexOf(appPath) === 0){
 					contentFilePath = path.join(appPath, content.file);
 				} else {
 					contentFilePath = path.join(rootPath, appPath, content.file);
